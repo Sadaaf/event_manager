f1c1844ff618518579db41299ff3e82435168992
Sadaf Al Arabi <sadafalarabi@mac.lan>
2025-03-20 10:15:40 -0400

    No idea what I just added but an api

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -17,6 +17,10 @@ puts 'Event Manager Initialized'
 
 # Use the csv library
 require 'csv'
+require 'google/apis/civicinfo_v2'
+
+civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
 
 def clean_zipcode(zipcode)
   zipcode = zipcode.to_s
@@ -27,5 +31,21 @@ contents = CSV.open('event_attendees.csv', headers: true, header_converters: :sy
 contents.each do |row|
   first_name, zipcode = row[:first_name] ,row[:zipcode]
   zipcode = clean_zipcode(zipcode)
-  puts "#{first_name} #{zipcode}"
+
+  begin
+  legislators = civic_info.representative_info_by_address(
+    address: zipcode,
+    levels: 'country',
+    roles: ['legislatorUpperBody', 'legislatorLowerBody']
+  )
+
+  legislators = legislators.officials
+
+  rescue
+    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+  end
+
+  puts "#{first_name} #{zipcode} #{legislators}"
+
+  break
 end
