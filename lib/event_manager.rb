42d77f80c2d00c7c5aefe4567bdc1fb37db66c76
Sadaf Al Arabi <sadafalarabi@mac.home>
2025-03-22 04:20:45 -0400

    added option to save user registration times to txt file

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -19,9 +19,21 @@ puts 'Event Manager Initialized'
 require 'csv'
 require 'google/apis/civicinfo_v2'
 require 'erb'
+require 'time'
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5,'0')[0..4]
+end
+
+def clean_phone_number(phone_number)
+  phone_number = phone_number.to_s.gsub(/\D/, '')
+  if phone_number.length == 10
+    phone_number
+  elsif phone_number.length == 11 && phone_number[0] == '1'
+    phone_number = phone_number[1..10]
+  else
+    phone_number = '0000000000'
+  end
 end
 
 def legislators_by_zipcode(zip)
@@ -52,11 +64,27 @@ def save_thank_you_letter(id, form_letter)
   end
 end
 
+def save_registration_time(registration_time)
+  file_name = 'output/registration_times.txt'
+  Dir.mkdir('output') unless Dir.exist?('output')
+  File.open(file_name, 'a') do |file|
+    file.puts registration_time.strftime("%H:%M")
+  end
+end
+
+def find_registration_time(registration_date)
+  registration_date = Time.parse(registration_date.split(' ')[1])
+end
+
 contents.each do |row|
   id = row[0]
-  name, zipcode = row[:first_name] ,row[:zipcode]
+  registration_date = row[:regdate]
+  name, zipcode, phone_number = row[:first_name] ,row[:zipcode], row[:homephone]
   zipcode = clean_zipcode(zipcode)
+  phone_number = clean_phone_number(phone_number)
   legislators = legislators_by_zipcode(zipcode)
   form_letter = erb_template.result(binding)
+  registration_time = find_registration_time(registration_date)
+  save_registration_time(registration_time)
   save_thank_you_letter(id, form_letter)
 end
